c87fb260d34ff094a7b55ea3fe081faf384df716
TayroniAdriel <33350379+TayroniAdriel@users.noreply.github.com>
2024-12-03 16:57:17 +0000

    add the scripts `lint:check e` `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A project to learn how to build an application from scratch.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Shut up! 🤫. Just buy bitcoin! </h1>
+function Home() {
+  return <h1>Shut up! 🤫. Just buy bitcoin! </h1>;
 }
 
 export default Home;
